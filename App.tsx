bb540033ec8c25cf7b1e345a313c6ea3a1393884
ImIrish98 <dean99992011@gmail.com>
2023-12-29 01:49:58 +0000

    ART SHIT

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>hello</Text>
+      <Text>ART SHIT</Text>
       <StatusBar style="auto" />
     </View>
   );
